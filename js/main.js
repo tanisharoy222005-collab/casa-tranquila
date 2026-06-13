4dcff4300af3854b7cfeef1feac166da90dbf325
tanisharoy222005-collab <tanisharoy222005@gmail.com>
2026-06-14 01:38:57 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,46 @@
+/* =====================================
+   EXPERIENCES CAROUSEL
+===================================== */
+
 const experienceSlider = document.getElementById("experience-grid");
 
 const nextBtn = document.querySelector(".next-btn");
 const prevBtn = document.querySelector(".prev-btn");
 
-if(nextBtn && prevBtn){
+if (experienceSlider && nextBtn && prevBtn) {
 
     nextBtn.addEventListener("click", () => {
-
         experienceSlider.scrollBy({
             left: 420,
             behavior: "smooth"
         });
-
     });
 
     prevBtn.addEventListener("click", () => {
-
         experienceSlider.scrollBy({
             left: -420,
             behavior: "smooth"
         });
-
     });
 
 }
+
+/* =====================================
+   GALLERY CAROUSEL
+===================================== */
+
+const gallerySlider = document.getElementById("gallery-grid");
+
+const nextGallery = document.querySelector(".next-gallery");
+const prevGallery = document.querySelector(".prev-gallery");
+
+if (gallerySlider && nextGallery && prevGallery) {
+
+    nextGallery.addEventListener("click", () => {
+        gallerySlider.scrollBy({
+            left: 420,
+            behavior: "smooth"
+        });
+    });
+
+    prevGallery.addEvent
